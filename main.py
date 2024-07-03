a893dd1d5215ea8821657c869a395d61ac682c1e
Suiphobie <93397145+Suiphobie@users.noreply.github.com>
2024-07-03 11:05:50 +0200

    add text

--- main.py
+++ main.py
@@ -6,7 +6,28 @@ from folium.plugins import MarkerCluster
 import geopandas as gpd
 
 st.set_page_config(layout="wide")
-st.write('La France Absentionniste et ses variables explicatives')
+st.markdown("# Étude sur l'Abstention et ses Variables Explicatives.")
+st.markdown("Cette carte représente une étude croisée sur l'abstention électorale et ses possibles variables explicatives.")
+introcol1, introcol2 = st.columns(2)
+
+with introcol1:
+    st.markdown("""
+    
+    ## Volets de l'étude
+    - **Exploration des Données d'Abstention**: Analyse via Power BI des données de l'abstention par localisation (département, commune, etc.).
+    - **Carte Interactive Multicouches**:
+      - *Couche fonctionnelle actuelle* : **Indice de Position Sociale (IPS)** des établissements scolaires (collèges et lycées). Cette couche permet de visualiser tous les établissements ayant un IPS inférieur à 100 et de les comparer avec le taux d'abstention par département.
+    """)
+
+with introcol2:
+    st.markdown(""" 
+    ## Prochaines Évolutions
+    - Ajout de nouvelles couches sur la carte interactive :
+      - Taux de chômage par département.
+      - Répartition démographique de la population.
+      - Accès aux infrastructures culturelles.
+    """)
+
 
 @st.cache_data
 def load_data():
@@ -39,6 +60,7 @@ departement, etablissements_gdf = load_and_prepare_geojson(etablissements)
 
 # Widgets for filtering data
 with st.sidebar:
+    st.markdown('# La France Abstentionniste et ses variables explicatives')
     st.write('Filtre de la carte')
     annee_selectionnee = st.selectbox(
         'Choisir l\'année de la rentrée scolaire:',
@@ -53,6 +75,32 @@ with st.sidebar:
         'Choisir une élection',
         options=election_data['id_election'].unique()
     )
+    st.markdown("""
+    Vous retrouverez tous les datasets utilisés :
+    - [Annuaire de l'Éducation nationale](https://data.education.gouv.fr/explore/dataset/fr-en-annuaire-education/table/?disjunctive.type_etablissement&disjunctive.code_postal&disjunctive.nom_commune&disjunctive.code_departement&disjunctive.appartenance_education_prioritaire&disjunctive.libelle_academie&disjunctive.libelle_region&disjunctive.ministere_tutelle)
+    - [Contours des départements français issus d'OpenStreetMap](https://www.data.gouv.fr/fr/datasets/contours-des-departements-francais-issus-d-openstreetmap/)
+    - [Données des élections agrégées](https://www.data.gouv.fr/fr/datasets/donnees-des-elections-agregees/)
+    - [IPS des établissements scolaires (collèges & lycées)](https://data.education.gouv.fr/explore/?sort=modified&q=IPS)
+    - [Bureaux de vote et adresses de leurs électeurs](https://www.data.gouv.fr/fr/datasets/bureaux-de-vote-et-adresses-de-leurs-electeurs/)
+    - [Communes de france - Base  des codes postaux](https://www.data.gouv.fr/fr/datasets/communes-de-france-base-des-codes-postaux/)
+    """)
+
+    st.markdown("""
+    # Crédits de la Carte
+
+    Cette carte a été imaginée par :
+    - [Tahina Duroussy](https://www.linkedin.com/in/tahinaduroussy/)
+    - [Christelle Emery](https://www.linkedin.com/in/christelleemery/)
+    - [Thibault Le Balier](https://www.linkedin.com/in/thibaultlebalier/)
+    - [Armelle Tchoua](https://www.linkedin.com/in/armelletchoua/)
+    - [Olivier Schneider](https://www.linkedin.com/in/olivierschneider/)
+    - [Laura Moy](https://www.linkedin.com/in/lauramoy/)
+    - [Nasstya Fakhreddine](https://www.linkedin.com/in/nasstyafakhreddine/)
+
+    Et a été développée par :
+    - [Anoussone Simuong](https://www.linkedin.com/in/anousimuong/)
+    - [Melanie Orellana](https://www.linkedin.com/in/melanieorellana/)
+    """)
 
 # Filter establishments by year, type, and IPS
 etablissements_filtrés = etablissements_gdf[
@@ -132,3 +180,17 @@ folium.LayerControl(collapsed=False).add_to(map)
 # Display the map in Streamlit
 folium_static(map, width=1200, height=800)
 
+st.markdown(""" ### Note
+Les données et les couches ajoutées permettront d'approfondir l'analyse de l'abstention et de ses corrélations avec différents facteurs socio-économiques.
+""")
+
+st.write("Découvrez un tableau power BI avec des analyse sur l'abstentions par  géolocalisation dans notre étude complete")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image('image1.png')
+with col2:
+    st.image('image2.png')
+
+st.button('Accéder à la version complète')
